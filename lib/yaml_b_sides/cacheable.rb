5152f10e70ddba8deb75ef1beaf70c5902b0e7ef
Greg Orlov <gaorlov@gmail.com>
2016-08-30 22:37:09 -0700

    adding missing file

---
+++ lib/yaml_b_sides/cacheable.rb
@@ -0,0 +1,17 @@
+module YamlBSides
+  module Cacheable
+    extend ActiveSupport::Concern
+
+    included do
+      class << self
+        def cache_key
+          @cache_key ||= Digest::SHA256.hexdigest(@data.to_json)
+        end
+      end
+
+      def cache_key
+        @cache_key ||= Digest::SHA256.hexdigest(@atrtibutes.to_json)
+      end
+    end
+  end
+end
